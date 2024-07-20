79ccee6be0e93a8430f8b21bc347192232e9ea79
Priyadharshini S <priyadharshinis5102@gmail.com>
2024-07-21 01:08:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,131 @@
+import { useState,useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [answer, setAnswer] = useState(false);
+  const [count, setCount] = useState(0);
+  const [img, setImage] = useState("/src/assets/puuung-holding-hands.gif");
+  const [icon,setIcon] = useState("/src/assets/pause.png")
+  const [a, setA] = useState(100);
+  const [b, setB] = useState(50);
+  const [aud,setAud] = useState(new Audio("/Everything-I-Do.mp3"));
+  aud.loop = true;
+  
+  const handleClickNo = () => {
+       
+   
+    const no = document.getElementById("noButton");
+    var x = Math.round(
+      Math.random() * Math.abs(window.innerWidth - no.offsetWidth) - 85
+    );
+    var y = Math.round(
+      Math.random() * Math.abs(window.innerHeight - no.offsetHeight) - 48
+    );
+    no.style.left = `${x}px`;
+    no.style.top = `${y}px`;
+
+    const yes = document.getElementById("yesButton");
+    yes.style.width = `${a}px`;
+    yes.style.height = `${b}px`;
+
+    setA((a) => a + 10);
+    setB((b) => b + 10);
+    if (count == 25) {
+      setImage("/src/assets/puuung-sad.gif");
+      setAnswer(true);
+    } else {
+      setCount((count) => count + 1);
+    }
+  };
+
+   useEffect(() => {
+    const requestAudioPermission = async () => {
+      try {
+        await navigator.mediaDevices.getUserMedia({ audio: true });
+        aud.play(); // Play audio if permission granted
+      } catch (error) {
+        console.error("Error requesting audio permission or playing audio:", error);
+        // Provide message to user if consent denied or error occurred
+      }
+    };
+
+    requestAudioPermission();
+  }, []);
+
+  return (
+    <>
+      <div className="flex gap-[10px]">
+        <button
+          className="absolute right-[75px] top-0 m-[10px]"
+          onClick={() => {
+         
+            if (aud.paused &&  !aud.ended  ) {
+              aud.loop = true;
+              aud.play();
+              setIcon("/src/assets/pause.png")
+            } else {
+              aud.pause();
+              setIcon("/src/assets/play.png")
+            }
+          }}
+        >
+        <img src={icon} className="w-[50px] h-[50px]"></img>
+       
+          
+        </button>
+        {/* <button
+          className="absolute right-0 top-0 m-[10px]"
+          onClick={() => {
+            audio.loop = false;
+            audio.pause();
+            
+          }}
+        >
+         
+        </button> */}
+      </div>
+      {answer ? (
+        <div className="h-screen  flex flex-col justify-center items-center bg-[#fcfefc]">
+          <img src={img} className="w-[300px] h-[500px] rounded-[50px]"></img>
+        </div>
+      ) : (
+        <div
+          className='flex flex-col justify-center items-center z-[1] h-screen bg-[url("/src/assets/heart.gif")] bg-no-repeat bg-center '
+     
+        >
+          <img
+            src="/src/assets/cupid.gif"
+            className="w-[200px] h-[200px] m-[25px]"
+          ></img>
+          <h2 className="text-black text-5xl">Will you go out with me?</h2>
+          <div className="flex gap-[25px] m-10">
+            <button
+              id="yesButton"
+              className={`bg-yellow-300 rounded-lg w-[${a}px] h-[${b}px]  text-2xl hover:bg-yellow-200`}
+              onClick={() => setAnswer((answer) => true)}
+            >
+              Yes
+            </button>
+            <button
+              id="noButton"
+              className={` bg-purple-300 rounded-lg w-[100px] h-[50px] text-2xl hover:bg-purple-200 `}
+              onClick={() => handleClickNo()}
+              onMouseOver={() => {
+                handleClickNo()
+            
+                
+              }}
+            >
+              No
+            </button>
+          </div>
+        
+        </div>
+      )}
+
+   
+    </>
+  );
+}
+
+export default App;
